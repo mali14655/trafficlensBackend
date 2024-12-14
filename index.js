880fb2480d53b0e5b60787e2504770edd3953c27
Muhammad Ali <mali146643@gmail.com>
2024-12-14 17:54:51 +0500

    mongoDB not connected

--- index.js
+++ index.js
@@ -11,25 +11,18 @@ const PORT=3000;
 const url=`mongodb+srv://mali146643:${process.env.PASSWORD}@cluster0.jbbwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 //Connection
-ConnectToMongoDb(url)
-.then(()=>{
-    console.log("Successfully Connected !")
-})
-.catch((err)=>{
-    console.log(err);
-})
+// ConnectToMongoDb(url)
+// .then(()=>{
+//     console.log("Successfully Connected !")
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
 
 
 //middlewares
 app.use(express.json());
-app.use(cors({
-    // origin:"http://localhost:5173"
-    origin:"https://trafficlens-frontend.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    preflightContinue: false,  // Ensure preflight requests are handled
-    optionsSuccessStatus: 204, // For older browsers
-}))
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log(`Request Method: ${req.method}, Path: ${req.path}`);
