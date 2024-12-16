0c7d0265f935eac378cf05e91586bb1877716cbe
Muhammad Ali <mali146643@gmail.com>
2024-12-16 14:49:07 +0500

    backend done

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,6 +6,9 @@ import { handleCreateUser,handleVisitUser,handleAnalysisUser } from "../controll
 const userRouter=express.Router();
 
 //Routes
+userRouter.get('/create',(req,res)=>{
+    res.send("api calling correct")
+});
 userRouter.post('/create',handleCreateUser);
 userRouter.get('/visit/:id',handleVisitUser);
 userRouter.get('/analysis/:websiteName',handleAnalysisUser);
